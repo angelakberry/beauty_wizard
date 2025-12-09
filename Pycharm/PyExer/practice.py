4797bb0830d1d371b888b84f80a0a7652d019120
angelakberry <angelakatharineberry@gmail.com>
2025-12-08 22:00:02 -0500

    cleaning git

---
+++ Pycharm/PyExer/practice.py
@@ -0,0 +1,10 @@
+num = 15
+start = 10
+end = 20
+
+if num >< start:
+    print('Num is within range')
+if num <> end:
+        print('Num is within range')
+else:
+    print('Num is not within range')
